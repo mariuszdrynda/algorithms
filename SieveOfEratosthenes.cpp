12dfe527ca7894a5c55ca7bb46347a4ddacf87ff
Mariusz Drynda <mariuszdrynda@gmail.com>
2024-04-19 11:17:42 +0200

    Sieve Of Eratosthenes

---
+++ SieveOfEratosthenes.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+int sieve(const int count){
+    if(count < 2) return 0;
+    std::vector<bool> v(count, false);
+    for(int p = 2; p * p <= count;){
+        for(int i = 2*p ;i <= count; i += p){
+            v[i] = true;
+        }
+        do{
+            p++;
+        }while(v[p]);
+    }
+    int result = 0;
+    for(int i = 2; i <= count; i++){
+        if(v[i] == false) result++;
+    }
+    return result;
+}
+int main(){
+    std::cout << sieve(1000000);
+}
